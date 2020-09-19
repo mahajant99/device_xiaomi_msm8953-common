f817f03aa9d2b157315bbca2308f516f79a632d0
Luchetto <lucchetto.tie@live.com>
2020-09-19 21:53:49 +0530

    msm8953-commn: Copy vndk-29 libc++ to vendor
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -354,6 +354,9 @@ PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libc++.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libc++.so
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
