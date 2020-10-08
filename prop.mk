bd357e03d394fd79cf153137298f3a76351e2f69
Quallenauge <Hamsi2k@freenet.de>
2020-10-08 22:32:01 +0200

    msm8953-common: Force enable bpf.
    Our device was launched with Android N (kernel 3.18).
    This causes the BpfUtils to not use bpf.
    Since we upgraded to kernel 4.9, this detection doesn't work,
    but on R there's an switch, set via property ro.kernel.ebpf.supported
    to force enbale 4.9 bpf mode.
    
    Change-Id: I263e3aaa5a9a63cd027d8750c7cad0f34f432ed2
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- prop.mk
+++ prop.mk
@@ -45,6 +45,23 @@ qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
 ro.qualcomm.bt.hci_transport=smd
 
+# Bpf
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.kernel.ebpf.supported=1
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+camera.display.lmax=1280x720 \
+camera.display.umax=1920x1080 \
+camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
+camera.lowpower.record.enable=1 \
+media.camera.ts.monotonic=1 \
+persist.camera.gyro.disable=0 \
+persist.camera.isp.clock.optmz=0 \
+persist.camera.stats.test=5 \
+persist.vendor.qti.telephony.vt_cam_interface=1 \
+vidc.enc.dcvs.extra-buff-count=2
+
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
 ro.charger.disable_init_blank=true \
