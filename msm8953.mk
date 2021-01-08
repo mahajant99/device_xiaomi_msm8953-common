3d4caac396c6210e937be4307c96270fe4ad24f3
Michael Bestas <mkbestas@lineageos.org>
2021-01-08 19:12:33 +0200

    msm8953-common: Build libgui_vendor
    Change-Id: I69dc13d9d4c5dc20fd580be5d7816f9c62397f94
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -368,6 +368,9 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libgui_vendor
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so \
     prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so \
