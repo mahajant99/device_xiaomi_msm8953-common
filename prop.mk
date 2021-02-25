6788338d4cf7cf57cfa5d57cd692e5892f38edbf
ItsVixano <vixanonew@gmail.com>
2021-02-25 12:32:35 +0100

    msm8953-common: Import missing SurfaceFlinger props from sunfish
    Change-Id: I54901406fa31d4569cb0bbf19ef648b70fbf960e

--- prop.mk
+++ prop.mk
@@ -221,7 +221,9 @@ persist.rcs.supported=1
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.protected_contents=true
+ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.early_phase_offset_ns=1500000 \
